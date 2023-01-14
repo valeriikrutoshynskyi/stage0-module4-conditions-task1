03f87ea1a5446fe899c9d50c60e494c66fb4f4c9
Valerii Krutoshynskyi <valera9496@gmail.com>
2023-01-14 21:10:01 +0100

    Done 4.1

--- src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task1;
 
 public class PositiveNumberDeterminer {
     public void isPositive(int numberToBeDetermined) {
+        int x = numberToBeDetermined;
+        if (x > 0){
+            System.out.println(true);
+        }
+        else {
+            System.out.println(false);
+        }
     }
 }
